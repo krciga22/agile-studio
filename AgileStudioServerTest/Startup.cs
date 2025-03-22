0492103a341d95619244f577745444f16cf68fae
Andrew <krciga22@gmail.com>
2025-03-21 20:09:25 -0500

    [server] Cleanup Startup.cs

--- AgileStudioServerTest/Startup.cs
+++ AgileStudioServerTest/Startup.cs
@@ -2,32 +2,13 @@
 using AgileStudioServerTest.IntegrationTests;
 using Microsoft.Extensions.DependencyInjection;
 using AgileStudioServer.Core.Hydrator;
-using AgileStudioServer.CoreFeatures.Sprints.APIs;
-using AgileStudioServer.CoreFeatures.Sprints.APIs.DTOs.Hydrators;
-using AgileStudioServer.CoreFeatures.Sprints.Services;
-using AgileStudioServer.CoreFeatures.Releases.APIs;
-using AgileStudioServer.CoreFeatures.Releases.APIs.DTOs.Hydrators;
-using AgileStudioServer.CoreFeatures.Releases.Services;
-using AgileStudioServer.CoreFeatures.Releases.Services.Models.Hydrators;
-using AgileStudioServer.CoreFeatures.Releases.Repositories.Entities.Hydrators;
-using AgileStudioServer.CoreFeatures.Workflows.APIs;
-using AgileStudioServer.CoreFeatures.Projects.APIs;
-using AgileStudioServer.CoreFeatures.Projects.APIs.DTOs.Hydrators;
-using AgileStudioServer.CoreFeatures.Workflows.APIs.DTOs.Hydrators;
-using AgileStudioServer.CoreFeatures.BacklogItems.APIs;
-using AgileStudioServer.CoreFeatures.BacklogItems.APIs.DTOs.Hydrators;
-using AgileStudioServer.CoreFeatures.Users.APIs.DTOs.Hydrators;
-using AgileStudioServer.CoreFeatures.Workflows.Services;
-using AgileStudioServer.CoreFeatures.Workflows.Services.Models.Hydrators;
-using AgileStudioServer.CoreFeatures.Projects.Services;
-using AgileStudioServer.CoreFeatures.Projects.Services.Models.Hydrators;
-using AgileStudioServer.CoreFeatures.Users.Services;
-using AgileStudioServer.CoreFeatures.Users.Services.Models.Hydrators;
-using AgileStudioServer.CoreFeatures.BacklogItems.Services;
-using AgileStudioServer.CoreFeatures.BacklogItems.Services.Models.Hydrators;
-using AgileStudioServer.CoreFeatures.Workflows.Repositories.Entities.Hydrators;
-using AgileStudioServer.CoreFeatures.Projects.Repositories.Entities.Hydrators;
-using AgileStudioServer.CoreFeatures.BacklogItems.Repositories.Entities.Hydrators;
+
+using BacklogItems = AgileStudioServer.CoreFeatures.BacklogItems;
+using Projects = AgileStudioServer.CoreFeatures.Projects;
+using Releases = AgileStudioServer.CoreFeatures.Releases;
+using Sprints = AgileStudioServer.CoreFeatures.Sprints;
+using Users = AgileStudioServer.CoreFeatures.Users;
+using Workflows = AgileStudioServer.CoreFeatures.Workflows;
 
 namespace AgileStudioServerTest
 {
@@ -39,14 +20,14 @@ namespace AgileStudioServerTest
                 DBContextFactory.ConfigureDefaultOptions(ref optionsBuilder);
             });
 
-            services.AddScoped<BacklogItemController>();
-            services.AddScoped<BacklogItemTypeController>();
-            services.AddScoped<BacklogItemTypeSchemaController>();
-            services.AddScoped<ProjectController>();
-            services.AddScoped<ReleaseController>();
-            services.AddScoped<SprintController>();
-            services.AddScoped<WorkflowController>();
-            services.AddScoped<WorkflowStateController>();
+            services.AddScoped<BacklogItems.APIs.BacklogItemController>();
+            services.AddScoped<BacklogItems.APIs.BacklogItemTypeController>();
+            services.AddScoped<BacklogItems.APIs.BacklogItemTypeSchemaController>();
+            services.AddScoped<Projects.APIs.ProjectController>();
+            services.AddScoped<Releases.APIs.ReleaseController>();
+            services.AddScoped<Sprints.APIs.SprintController>();
+            services.AddScoped<Workflows.APIs.WorkflowController>();
+            services.AddScoped<Workflows.APIs.WorkflowStateController>();
 
             services.AddScoped<ModelFixtures>();
             services.AddScoped<EntityFixtures>();
@@ -54,56 +35,56 @@ namespace AgileStudioServerTest
             services.AddScoped<Hydrator>();
             services.AddScoped<HydratorRegistry>();
 
-            services.AddScoped<IHydrator, BacklogItemDtoHydrator>();
-            services.AddScoped<IHydrator, BacklogItemSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, BacklogItemTypeDtoHydrator>();
-            services.AddScoped<IHydrator, BacklogItemTypeSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, BacklogItemTypeSchemaDtoHydrator>();
-            services.AddScoped<IHydrator, BacklogItemTypeSchemaSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, ProjectDtoHydrator>();
-            services.AddScoped<IHydrator, ProjectSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, ReleaseDtoHydrator>();
-            services.AddScoped<IHydrator, ReleaseSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, SprintDtoHydrator>();
-            services.AddScoped<IHydrator, SprintSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, UserSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, WorkflowDtoHydrator>();
-            services.AddScoped<IHydrator, WorkflowSummaryDtoHydrator>();
-            services.AddScoped<IHydrator, WorkflowStateDtoHydrator>();
-            services.AddScoped<IHydrator, WorkflowStateSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.APIs.DTOs.Hydrators.BacklogItemDtoHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.APIs.DTOs.Hydrators.BacklogItemSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.APIs.DTOs.Hydrators.BacklogItemTypeDtoHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.APIs.DTOs.Hydrators.BacklogItemTypeSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.APIs.DTOs.Hydrators.BacklogItemTypeSchemaDtoHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.APIs.DTOs.Hydrators.BacklogItemTypeSchemaSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, Projects.APIs.DTOs.Hydrators.ProjectDtoHydrator>();
+            services.AddScoped<IHydrator, Projects.APIs.DTOs.Hydrators.ProjectSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, Releases.APIs.DTOs.Hydrators.ReleaseDtoHydrator>();
+            services.AddScoped<IHydrator, Releases.APIs.DTOs.Hydrators.ReleaseSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, Sprints.APIs.DTOs.Hydrators.SprintDtoHydrator>();
+            services.AddScoped<IHydrator, Sprints.APIs.DTOs.Hydrators.SprintSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, Users.APIs.DTOs.Hydrators.UserSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, Workflows.APIs.DTOs.Hydrators.WorkflowDtoHydrator>();
+            services.AddScoped<IHydrator, Workflows.APIs.DTOs.Hydrators.WorkflowSummaryDtoHydrator>();
+            services.AddScoped<IHydrator, Workflows.APIs.DTOs.Hydrators.WorkflowStateDtoHydrator>();
+            services.AddScoped<IHydrator, Workflows.APIs.DTOs.Hydrators.WorkflowStateSummaryDtoHydrator>();
             
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Services.Models.Hydrators.BacklogItemHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Services.Models.Hydrators.BacklogItemTypeHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Services.Models.Hydrators.BacklogItemTypeSchemaHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Services.Models.Hydrators.ChildBacklogItemTypeHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Projects.Services.Models.Hydrators.ProjectHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Releases.Services.Models.Hydrators.ReleaseHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Sprints.Services.Models.Hydrators.SprintHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Users.Services.Models.Hydrators.UserHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Workflows.Services.Models.Hydrators.WorkflowHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Workflows.Services.Models.Hydrators.WorkflowStateHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Services.Models.Hydrators.BacklogItemHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Services.Models.Hydrators.BacklogItemTypeHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Services.Models.Hydrators.BacklogItemTypeSchemaHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Services.Models.Hydrators.ChildBacklogItemTypeHydrator>();
+            services.AddScoped<IHydrator, Projects.Services.Models.Hydrators.ProjectHydrator>();
+            services.AddScoped<IHydrator, Releases.Services.Models.Hydrators.ReleaseHydrator>();
+            services.AddScoped<IHydrator, Sprints.Services.Models.Hydrators.SprintHydrator>();
+            services.AddScoped<IHydrator, Users.Services.Models.Hydrators.UserHydrator>();
+            services.AddScoped<IHydrator, Workflows.Services.Models.Hydrators.WorkflowHydrator>();
+            services.AddScoped<IHydrator, Workflows.Services.Models.Hydrators.WorkflowStateHydrator>();
 
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Repositories.Entities.Hydrators.BacklogItemHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Repositories.Entities.Hydrators.BacklogItemTypeHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Repositories.Entities.Hydrators.BacklogItemTypeSchemaHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.BacklogItems.Repositories.Entities.Hydrators.ChildBacklogItemTypeHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Projects.Repositories.Entities.Hydrators.ProjectHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Releases.Repositories.Entities.Hydrators.ReleaseHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Sprints.Repositories.Entities.Hydrators.SprintHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Users.Repositories.Entities.Hydrators.UserHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Workflows.Repositories.Entities.Hydrators.WorkflowHydrator>();
-            services.AddScoped<IHydrator, AgileStudioServer.CoreFeatures.Workflows.Repositories.Entities.Hydrators.WorkflowStateHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Repositories.Entities.Hydrators.BacklogItemHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Repositories.Entities.Hydrators.BacklogItemTypeHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Repositories.Entities.Hydrators.BacklogItemTypeSchemaHydrator>();
+            services.AddScoped<IHydrator, BacklogItems.Repositories.Entities.Hydrators.ChildBacklogItemTypeHydrator>();
+            services.AddScoped<IHydrator, Projects.Repositories.Entities.Hydrators.ProjectHydrator>();
+            services.AddScoped<IHydrator, Releases.Repositories.Entities.Hydrators.ReleaseHydrator>();
+            services.AddScoped<IHydrator, Sprints.Repositories.Entities.Hydrators.SprintHydrator>();
+            services.AddScoped<IHydrator, Users.Repositories.Entities.Hydrators.UserHydrator>();
+            services.AddScoped<IHydrator, Workflows.Repositories.Entities.Hydrators.WorkflowHydrator>();
+            services.AddScoped<IHydrator, Workflows.Repositories.Entities.Hydrators.WorkflowStateHydrator>();
 
-            services.AddScoped<BacklogItemService>();
-            services.AddScoped<BacklogItemTypeService>();
-            services.AddScoped<BacklogItemTypeSchemaService>();
-            services.AddScoped<ChildBacklogItemTypeService>();
-            services.AddScoped<ProjectService>();
-            services.AddScoped<ReleaseService>();
-            services.AddScoped<SprintService>();
-            services.AddScoped<UserService>();
-            services.AddScoped<WorkflowService>();
-            services.AddScoped<WorkflowStateService>();
+            services.AddScoped<BacklogItems.Services.BacklogItemService>();
+            services.AddScoped<BacklogItems.Services.BacklogItemTypeService>();
+            services.AddScoped<BacklogItems.Services.BacklogItemTypeSchemaService>();
+            services.AddScoped<BacklogItems.Services.ChildBacklogItemTypeService>();
+            services.AddScoped<Projects.Services.ProjectService>();
+            services.AddScoped<Releases.Services.ReleaseService>();
+            services.AddScoped<Sprints.Services.SprintService>();
+            services.AddScoped<Users.Services.UserService>();
+            services.AddScoped<Workflows.Services.WorkflowService>();
+            services.AddScoped<Workflows.Services.WorkflowStateService>();
         }
     }
 }

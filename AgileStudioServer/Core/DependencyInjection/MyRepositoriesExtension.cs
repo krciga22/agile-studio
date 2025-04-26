a9a26ad4d85e5b2653d9d7f61cf29d99f26f3b77
Andrew <krciga22@gmail.com>
2025-04-26 14:29:46 -0500

    [server] Auto register repositories as services

--- AgileStudioServer/Core/DependencyInjection/MyRepositoriesExtension.cs
+++ AgileStudioServer/Core/DependencyInjection/MyRepositoriesExtension.cs
@@ -1,16 +1,5 @@
-﻿using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemLinkTypes;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemLinkTypeSchemaEntries;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemLinkTypeSchemas;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItems;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemTypes;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemTypeSchemas;
-using AgileStudioServer.CoreFeatures.BacklogItems.ChildBacklogItemTypes;
-using AgileStudioServer.CoreFeatures.Projects.Projects;
-using AgileStudioServer.CoreFeatures.Releases.Releases;
-using AgileStudioServer.CoreFeatures.Sprints.Sprints;
-using AgileStudioServer.CoreFeatures.Users.Users;
-using AgileStudioServer.CoreFeatures.Workflows.Workflows;
-using AgileStudioServer.CoreFeatures.Workflows.WorkflowStates;
+﻿using AgileStudioServer.Core.Repositories;
+using System.Reflection;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -19,19 +8,16 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddMyRepositories(
              this IServiceCollection services)
         {
-            services.AddScoped<BacklogItemLinkTypeRepository>();
-            services.AddScoped<BacklogItemLinkTypeSchemaEntryRepository>();
-            services.AddScoped<BacklogItemLinkTypeSchemaRepository>();
-            services.AddScoped<BacklogItemRepository>();
-            services.AddScoped<BacklogItemTypeRepository>();
-            services.AddScoped<BacklogItemTypeSchemaRepository>();
-            services.AddScoped<ChildBacklogItemTypeRepository>();
-            services.AddScoped<ProjectRepository>();
-            services.AddScoped<UserRepository>();
-            services.AddScoped<ReleaseRepository>();
-            services.AddScoped<SprintRepository>();
-            services.AddScoped<WorkflowRepository>();
-            services.AddScoped<WorkflowStateRepository>();
+            var classCollection = Assembly.GetExecutingAssembly()
+                            .DefinedTypes.Where(t =>
+                                t.IsSubclassOf(typeof(Repository)) &&
+                                t.IsPublic &&
+                                !t.IsAbstract);
+
+            foreach (var classType in classCollection)
+            {
+                services.AddScoped(classType);
+            }
 
             return services;
         }

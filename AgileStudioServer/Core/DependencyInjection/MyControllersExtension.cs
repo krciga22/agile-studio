bb5b0f4503db32010ba1a9a5026e0648436da938
Andrew <krciga22@gmail.com>
2025-04-26 13:58:11 -0500

    [server] Auto register controllers as services

--- AgileStudioServer/Core/DependencyInjection/MyControllersExtension.cs
+++ AgileStudioServer/Core/DependencyInjection/MyControllersExtension.cs
@@ -1,14 +1,5 @@
-﻿using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemLinkTypes;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemLinkTypeSchemaEntries;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemLinkTypeSchemas;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItems;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemTypes;
-using AgileStudioServer.CoreFeatures.BacklogItems.BacklogItemTypeSchemas;
-using AgileStudioServer.CoreFeatures.Projects.Projects;
-using AgileStudioServer.CoreFeatures.Releases.Releases;
-using AgileStudioServer.CoreFeatures.Sprints.Sprints;
-using AgileStudioServer.CoreFeatures.Workflows.Workflows;
-using AgileStudioServer.CoreFeatures.Workflows.WorkflowStates;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -17,17 +8,16 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddMyControllers(
              this IServiceCollection services)
         {
-            services.AddScoped<BacklogItemController>();
-            services.AddScoped<BacklogItemTypeController>();
-            services.AddScoped<BacklogItemTypeSchemaController>();
-            services.AddScoped<BacklogItemLinkTypeController>();
-            services.AddScoped<BacklogItemLinkTypeSchemaController>();
-            services.AddScoped<BacklogItemLinkTypeSchemaEntryController>();
-            services.AddScoped<ProjectController>();
-            services.AddScoped<ReleaseController>();
-            services.AddScoped<SprintController>();
-            services.AddScoped<WorkflowController>();
-            services.AddScoped<WorkflowStateController>();
+            var classCollection = Assembly.GetExecutingAssembly()
+                            .DefinedTypes.Where(t => 
+                                t.IsSubclassOf(typeof(ControllerBase)) &&
+                                t.IsPublic && 
+                                !t.IsAbstract);
+
+            foreach (var classType in classCollection)
+            {
+                services.AddScoped(classType);
+            }
 
             return services;
         }

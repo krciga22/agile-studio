dcbce3f3079fe2d952dfb795b7f6a4ddc6aee293
Andrew <krciga22@gmail.com>
2025-08-31 18:48:18 -0500

    [server] Show swager UI in all environments

--- AgileStudioServer/Program.cs
+++ AgileStudioServer/Program.cs
@@ -21,21 +21,12 @@ namespace AgileStudioServer
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.MapControllers();
-
             app.Run();
         }
     }

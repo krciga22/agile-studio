8a7e4c6882ee4e246528603d5d4383006161fcaf
Andrew <krciga22@gmail.com>
2026-01-30 23:35:53 -0500

    [server] Add bearer token authorization to swagger docs

--- AgileStudioServer/Core/DependencyInjection/MySwaggerGenExtension.cs
+++ AgileStudioServer/Core/DependencyInjection/MySwaggerGenExtension.cs
@@ -15,6 +15,31 @@ namespace Microsoft.Extensions.DependencyInjection
                     Title = Constants.ProductName, 
                     Version = $"v{Constants.ProductVersion}"
                 });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT Access Token (without the 'Bearer' prefix)."
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
 
             return services;

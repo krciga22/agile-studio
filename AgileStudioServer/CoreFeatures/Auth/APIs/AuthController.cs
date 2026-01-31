8563e2ad3b0fdf11523126f467c240e2fb82c296
Andrew <krciga22@gmail.com>
2026-01-30 23:39:30 -0500

    [server] Remove Auth/Login and Auth/GetAccessToken endpoints
    The client authentications directly with authorization server for now, so the API doesn't need authentication related endpoints for now.

--- AgileStudioServer/CoreFeatures/Auth/APIs/AuthController.cs
+++ AgileStudioServer/CoreFeatures/Auth/APIs/AuthController.cs
@@ -1,5 +1,4 @@
 using AgileStudioServer.CoreFeatures.Auth.APIs.DTOs;
-using Auth0.AspNetCore.Authentication;
 using Auth0.AuthenticationApi;
 using Auth0.AuthenticationApi.Models;
 using Microsoft.AspNetCore.Authentication;
@@ -20,50 +19,6 @@ namespace AgileStudioServer.CoreFeatures.Auth.APIs
         {
             HostEnvironment = hostEnvironment;
             Config = config;
-        }
-
-        /// <summary>
-        /// Primary authentication endpoint for the web application.
-        /// 
-        /// Unauthenticated users will be redirected to an Authentication 
-        /// Provider to authenticate. Once authenticated, they will 
-        /// then be redirected back to the "/" index route or to the route 
-        /// they previously requrested and which required authorization.
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("Login", Name = "AuthLogin")]
-        public async Task Login()
-        {
-            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
-                .Build();
-
-            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-        }
-
-        /// <summary>
-        /// Get the currently authenticated user's access token (for 
-        /// use in the development environment only).
-        /// </summary>
-        /// <returns>The access token</returns>
-        [HttpGet("GetAccessToken", Name = "AuthGetAccessToken")]
-        [ProducesResponseType(typeof(ForbidResult), StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(typeof(NotFoundResult), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(OkResult), StatusCodes.Status200OK)]
-        [Authorize]
-        public async Task<IActionResult> GetAccessToken()
-        {
-            if (!HostEnvironment.IsDevelopment())
-            {
-                return Forbid();
-            }
-
-            var token = await HttpContext.GetTokenAsync(Auth0Constants.AuthenticationScheme, "access_token");
-            if (token is null || token == string.Empty)
-            {
-                return NotFound();
-            }
-
-            return Ok(token);
         }
 
         /// <summary>

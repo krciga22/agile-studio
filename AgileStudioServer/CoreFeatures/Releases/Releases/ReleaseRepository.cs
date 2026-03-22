c7a96f0e6c626c329940633f1e0758272f11ba93
Andrew <krciga22@gmail.com>
2026-03-21 22:52:15 -0500

    [server] Add resource repository methods to ReleaseRepository

--- AgileStudioServer/CoreFeatures/Releases/Releases/ReleaseRepository.cs
+++ AgileStudioServer/CoreFeatures/Releases/Releases/ReleaseRepository.cs
@@ -1,11 +1,15 @@
 ﻿using AgileStudioServer.Core.Hydrator;
+using AgileStudioServer.Core.Pagination;
 using AgileStudioServer.Core.Repositories;
+using AgileStudioServer.Core.Services;
+using AgileStudioServer.CoreFeatures.Resources.Resource;
+using AgileStudioServer.CoreFeatures.Resources.Resource.Exceptions;
 using AgileStudioServer.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace AgileStudioServer.CoreFeatures.Releases.Releases
 {
-    public class ReleaseRepository : EntityRepository<DBContext, ReleaseModel, Release, int>
+    public class ReleaseRepository : EntityRepository<DBContext, ReleaseModel, Release, int>, IResourceRepository
     {
         public ReleaseRepository(DBContext dbContext, Hydrator hydrator) : base(dbContext, hydrator)
         {
@@ -23,6 +27,66 @@ namespace AgileStudioServer.CoreFeatures.Releases.Releases
                 release.Project.ID == projectId).ToList();
 
             return HydrateModels(entities);
+        }
+
+        public string GetResourceType()
+        {
+            return ResourceTypes.ReleasesRelease;
+        }
+
+        public Type GetResourceDtoType()
+        {
+            return typeof(ReleaseDto);
+        }
+
+        public Type GetResourceDtoCreateType()
+        {
+            return typeof(ReleasePostDto);
+        }
+
+        public Type GetResourceDtoUpdateType()
+        {
+            return typeof(ReleasePatchDto);
+        }
+
+        public Type GetResourceModelType()
+        {
+            return typeof(ReleaseModel);
+        }
+
+        public PaginationResults<object> GetAllResources(ServiceContext serviceContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object? GetResource(int id)
+        {
+            return Get(id);
+        }
+
+        public bool IsResource(int id)
+        {
+            return Exists(id);
+        }
+
+        public object CreateResource(object model)
+        {
+            return Create((ReleaseModel) model);
+        }
+
+        public object UpdateResource(int id, object model)
+        {
+            if (id != GetIdentifier((ReleaseModel) model))
+            {
+                throw new ResourceIdentifierMismatchException(id);
+            }
+
+            return Update((ReleaseModel) model);
+        }
+
+        public void DeleteResource(object model)
+        {
+            Delete((ReleaseModel) model);
         }
 
         protected override DbSet<Release> GetDbSet()
--- AgileStudioServer/CoreFeatures/Resources/Resource/ResourceTypes.cs
+++ AgileStudioServer/CoreFeatures/Resources/Resource/ResourceTypes.cs
@@ -4,5 +4,7 @@ namespace AgileStudioServer.CoreFeatures.Resources.Resource
     internal class ResourceTypes
     {
         public const string ProjectsProject = "projects.project";
+
+        public const string ReleasesRelease = "releases.release";
     }
 }

6731ab0e555e578bcf98efa90ce2ca7801a736dc
Andrew <krciga22@gmail.com>
2026-04-04 12:48:16 -0500

    [server] Fix RoleGrantFixture using a role key that doesn't exist

--- AgileStudioServerTest/CoreFeatures/Auth/RoleGrants/RoleGrantFixture.cs
+++ AgileStudioServerTest/CoreFeatures/Auth/RoleGrants/RoleGrantFixture.cs
@@ -1,15 +1,18 @@
 using AgileStudioServer.CoreFeatures.Auth.RoleGrants;
 using AgileStudioServer.CoreFeatures.Users.Users;
 using AgileStudioServerTest.Core.Fixtures;
+using AgileStudioServerTest.CoreFeatures.Auth.Roles;
 using AgileStudioServerTest.CoreFeatures.Users.Users;
 
 namespace AgileStudioServerTest.CoreFeatures.Auth.RoleGrants
 {
     public class RoleGrantFixture(
         RoleGrantRepository roleGrantRepository,
-        UserFixture userFixture) : AbstractEntityFixture<RoleGrantRepository>(roleGrantRepository)
+        UserFixture userFixture,
+        RoleFixture roleFixture) : AbstractEntityFixture<RoleGrantRepository>(roleGrantRepository)
     {
         private readonly UserFixture _userFixture = userFixture;
+        private readonly RoleFixture _RoleFixture = roleFixture;
 
         public RoleGrantModel Create(
             string? roleKey = null,
@@ -19,10 +22,14 @@ namespace AgileStudioServerTest.CoreFeatures.Auth.RoleGrants
             string? scopeID = null,
             UserModel? createdBy = null)
         {
-            roleKey ??= "test-role";
             subjectType ??= RoleSubjectTypes.USER;
             createdBy ??= _userFixture.Create();
             subjectID ??= createdBy.ID.ToString();
+
+            if(roleKey == null){
+                var role = _RoleFixture.Create();
+                roleKey = role.RoleKey;
+            }
 
             var grant = new RoleGrantModel(roleKey, subjectType, subjectID)
             {

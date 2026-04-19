0331d444dab25ba7a941e8a3749540e0144a0a97
Andrew <krciga22@gmail.com>
2026-04-18 23:25:31 -0500

    [server] Bump version to 5.0

--- AgileStudioServer/Constants.cs
+++ AgileStudioServer/Constants.cs
@@ -4,7 +4,7 @@ namespace AgileStudioServer
     internal class Constants
     {
         public const string ProductName = "Agile Studio Server";
-        public const string ProductVersion = "4.0.0";
+        public const string ProductVersion = "5.0.0";
         public const int DefaultPage = 1;
         public const int ItemsPerPage = 2;
         public const int MaxItemsPerPage = 2;

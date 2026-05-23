47990822c8df0e9c257f6185d61b338e85482ec3
Andrew <krciga22@gmail.com>
2026-05-23 18:00:29 -0500

    [server] Set better defaults for ItemsPerPage and MaxItemsPerPage

--- AgileStudioServer/Constants.cs
+++ AgileStudioServer/Constants.cs
@@ -6,8 +6,8 @@ namespace AgileStudioServer
         public const string ProductName = "Agile Studio Server";
         public const string ProductVersion = "5.0.0";
         public const int DefaultPage = 1;
-        public const int ItemsPerPage = 2;
-        public const int MaxItemsPerPage = 2;
+        public const int ItemsPerPage = 10;
+        public const int MaxItemsPerPage = 50;
         public const string CorsPolicyDefault = "CorsPolicyDefault";
     }
 }

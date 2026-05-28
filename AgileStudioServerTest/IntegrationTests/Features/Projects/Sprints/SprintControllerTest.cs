d9f5b8a36501717993788633bb40ab4e4f997cc4
Andrew <krciga22@gmail.com>
2026-05-27 19:02:07 -0500

    [server] Rework sprint controller tests to use resource controller

--- AgileStudioServerTest/IntegrationTests/Features/Projects/Sprints/SprintControllerTest.cs
+++ AgileStudioServerTest/IntegrationTests/Features/Projects/Sprints/SprintControllerTest.cs
@@ -1,109 +1,114 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using AgileStudioServer.Features.Projects.Sprints;
+﻿using AgileStudioServer.Core.Services;
 using AgileStudioServer.Data;
+using AgileStudioServer.Features.Auth.Roles;
+using AgileStudioServer.Features.Projects.Sprints;
+using AgileStudioServer.Features.Resources.Resource;
 using AgileStudioServerTest.Features.Projects.Projects;
 using AgileStudioServerTest.Features.Projects.Sprints;
+using AgileStudioServerTest.Features.Users.Users;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace AgileStudioServerTest.IntegrationTests.Features.Projects.Sprints
 {
-    public class SprintControllerTest : AbstractControllerTest
+    public class SprintControllerTest : ResourceControllerTest
     {
+        private readonly ResourceController _ResourceController;
+
         private readonly SprintController _Controller;
 
         private readonly SprintFixture _SprintFixture;
 
         private readonly ProjectFixture _ProjectFixture;
 
+        private readonly UserFixture _UserFixture;
+
         public SprintControllerTest(
             DBContext dbContext,
+            ResourceController resourceController, // TODO move to controller test base class
             SprintController controller,
             SprintFixture sprintFixture,
-            ProjectFixture projectFixture) : base(dbContext)
+            ProjectFixture projectFixture,
+            UserFixture userFixture,
+            ServiceContext serviceContext,
+            IUrlHelperFactory? iUrlHelperFactory = null) : 
+            base(dbContext, serviceContext, iUrlHelperFactory)
         {
+            _ResourceController = resourceController;
             _Controller = controller;
             _SprintFixture = sprintFixture;
             _ProjectFixture = projectFixture;
+            _UserFixture = userFixture;
         }
 
         [Fact]
         public void Get_WithId_ReturnsDto()
         {
-            //var sprint = _SprintFixture.Create();
+            var user = _UserFixture.Create();
 
-            //SprintDto? sprintDto = null;
-            //IActionResult result = _Controller.Get(sprint.ID);
-            //if (result is OkObjectResult okResult)
-            //{
-            //    sprintDto = okResult.Value as SprintDto;
-            //}
+            var sprint = _SprintFixture.Create(createdBy: user);
+            _ProjectFixture.GrantAccess(sprint.ProjectID, user.ID, RoleKeys.PROJECTS_PROJECT_ADMIN);
 
-            //Assert.IsType<SprintDto>(sprintDto);
-            //Assert.Equal(sprint.ID, sprintDto.ID);
-        }
+            InitHttpAndServiceContextWithUser(user);
+            object[] id = [sprint.ID];
+            var result = _ResourceController.Get(
+                _HttpContext, ResourceTypes.SprintsSprint, id);
 
-        [Fact]
-        public void Get_WithInvalidId_ReturnsNotFoundResult()
-        {
-            //IActionResult result = _Controller.Get(Constants.NonExistantId);
+            var objectResult =
+                Assert.IsType<Ok<object>>(result);
 
-            //Assert.IsType<NotFoundResult>(result as NotFoundResult);
-        }
+            var sprintDtoResult =
+                Assert.IsType<SprintDto>(objectResult.Value);
 
-        [Fact]
-        public void Post_WithDto_ReturnsDto()
-        {
-            //var project = _ProjectFixture.Create();
-            //var sprintPostDto = new SprintPostDto(project.ID);
-            //sprintPostDto.Description = "Test Sprint";
-
-            //SprintDto? sprintDto = null;
-            //IActionResult result = _Controller.Post(sprintPostDto);
-            //if (result is CreatedResult createdResult)
-            //{
-            //    sprintDto = createdResult.Value as SprintDto;
-            //}
-
-            //Assert.IsType<SprintDto>(sprintDto);
-            //Assert.Equal(sprintPostDto.Description, sprintDto.Description);
+            Assert.Equal(sprint.ID, sprintDtoResult.ID);
         }
 
         [Fact]
         public void Patch_WithIdAndDto_ReturnsDto()
         {
-            //var sprint = _SprintFixture.Create();
-            //var description = $"Test Sprint {sprint.ID} Updated";
-            //var sprintPatchDto = new SprintPatchDto(sprint.ID)
-            //{
-            //    Description = description
-            //};
+            var user = _UserFixture.Create();
 
-            //IActionResult result = _Controller.Patch(sprint.ID, sprintPatchDto);
-            //SprintDto? sprintDto = null;
-            //if (result is OkObjectResult okObjectResult)
-            //{
-            //    sprintDto = okObjectResult.Value as SprintDto;
-            //}
+            var sprint = _SprintFixture.Create(createdBy: user);
 
-            //Assert.IsType<SprintDto>(sprintDto);
-            //Assert.Equal(sprintPatchDto.Description, sprintDto.Description);
+            _ProjectFixture.GrantAccess(
+                sprint.ProjectID, user.ID, RoleKeys.PROJECTS_PROJECT_ADMIN);
+
+            var sprintPatchDto = new SprintPatchDto(sprint.ID)
+            {
+                Description = "Updated Description"
+            };
+            object data = IntegrationTestsUtil.ConvertDtoToObject(sprintPatchDto);
+
+            InitHttpAndServiceContextWithUser(user);
+            object[] id = [sprint.ID];
+            var result = _ResourceController.Patch(
+                _HttpContext, ResourceTypes.SprintsSprint, id, data);
+
+            var objectResult =
+                Assert.IsType<Ok<object>>(result);
+
+            var sprintDtoResult =
+                Assert.IsType<SprintDto>(objectResult.Value);
+
+            Assert.Equal(sprintPatchDto.Description, sprintDtoResult.Description);
         }
 
         [Fact]
         public void Delete_WithId_ReturnsOkResult()
         {
-            //var sprint = _SprintFixture.Create();
+            var user = _UserFixture.Create();
 
-            //IActionResult result = _Controller.Delete(sprint.ID);
+            var sprint = _SprintFixture.Create(createdBy: user);
 
-            //Assert.IsType<OkResult>(result as OkResult);
-        }
+            _ProjectFixture.GrantAccess(
+                sprint.ProjectID, user.ID, RoleKeys.PROJECTS_PROJECT_ADMIN);
 
-        [Fact]
-        public void Delete_WithInvalidId_ReturnsNotFoundResult()
-        {
-            //IActionResult result = _Controller.Delete(Constants.NonExistantId);
+            InitHttpAndServiceContextWithUser(user);
+            object[] id = [sprint.ID];
+            var result = _ResourceController.Delete(
+                _HttpContext, ResourceTypes.SprintsSprint, id);
 
-            //Assert.IsType<NotFoundResult>(result as NotFoundResult);
+            Assert.IsType<Ok>(result);
         }
     }
 }

2a040d4ceaa4c190a51b24711c8eb19b667ce342
Andrew <krciga22@gmail.com>
2026-05-31 00:52:11 -0500

    [server] Create AccountType repository

---
+++ AgileStudioServer/Features/Accounts/AccountTypes/AccountTypeRepository.cs
@@ -0,0 +1,25 @@
+using AgileStudioServer.Core.Hydrator;
+using AgileStudioServer.Core.Repositories;
+using AgileStudioServer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgileStudioServer.Features.Accounts.AccountTypes
+{
+    public class AccountTypeRepository : EntityRepository<DBContext, AccountTypeModel, AccountType, int>
+    {
+        public AccountTypeRepository(DBContext dbContext, Hydrator hydrator) : base(dbContext, hydrator)
+        {
+
+        }
+
+        public override int GetIdentifier(AccountTypeModel model)
+        {
+            return model.ID;
+        }
+
+        protected override DbSet<AccountType> GetDbSet()
+        {
+            return _DBContext.AccountType;
+        }
+    }
+}

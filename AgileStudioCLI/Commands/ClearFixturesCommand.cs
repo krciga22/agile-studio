a32a1c0b886043d60d7580caab7ebc41dad68fa7
Andrew <krciga22@gmail.com>
2026-06-12 20:05:18 -0500

    [server] Fix ClearFixtures command not working due to deletion ordering
    When accounts are deleted, backlog item types related to those accounts are cascade deleted, then backlog item type schemas, some of which might be required by still existing project records.

--- AgileStudioCLI/Commands/ClearFixturesCommand.cs
+++ AgileStudioCLI/Commands/ClearFixturesCommand.cs
@@ -26,21 +26,24 @@ namespace AgileStudioCLI.Commands
 
         private async void RemoveAllFixtures()
         {
-            await _DBContext.Account.ForEachAsync(x => _DBContext.Account.Remove(x));
-            await _DBContext.AccountType.ForEachAsync(x => _DBContext.AccountType.Remove(x));
-            await _DBContext.Project.ForEachAsync(x => _DBContext.Project.Remove(x));
+            // delete project related entities
+            await _DBContext.BacklogItem.ForEachAsync(x => _DBContext.BacklogItem.Remove(x));
             await _DBContext.Release.ForEachAsync(x => _DBContext.Release.Remove(x));
             await _DBContext.Sprint.ForEachAsync(x => _DBContext.Sprint.Remove(x));
-            await _DBContext.BacklogItem.ForEachAsync(x => _DBContext.BacklogItem.Remove(x));
-            await _DBContext.BacklogItemType.ForEachAsync(x => _DBContext.BacklogItemType.Remove(x));
+            await _DBContext.Project.ForEachAsync(x => _DBContext.Project.Remove(x));
+
+            // delete account related entities
             await _DBContext.BacklogItemTypeSchema.ForEachAsync(x => _DBContext.BacklogItemTypeSchema.Remove(x));
             await _DBContext.ChildBacklogItemType.ForEachAsync(x => _DBContext.ChildBacklogItemType.Remove(x));
+            await _DBContext.BacklogItemType.ForEachAsync(x => _DBContext.BacklogItemType.Remove(x));
             await _DBContext.BacklogItemLinkTypeSchemaEntry.ForEachAsync(x => _DBContext.BacklogItemLinkTypeSchemaEntry.Remove(x));
-            await _DBContext.BacklogItemLinkType.ForEachAsync(x => _DBContext.BacklogItemLinkType.Remove(x));
             await _DBContext.BacklogItemLinkTypeSchema.ForEachAsync(x => _DBContext.BacklogItemLinkTypeSchema.Remove(x));
+            await _DBContext.BacklogItemLinkType.ForEachAsync(x => _DBContext.BacklogItemLinkType.Remove(x));
             await _DBContext.WorkflowState.ForEachAsync(x => _DBContext.WorkflowState.Remove(x));
             await _DBContext.Workflow.ForEachAsync(x => _DBContext.Workflow.Remove(x));
-            await _DBContext.User.ForEachAsync(x => _DBContext.User.Remove(x));
+            await _DBContext.Account.ForEachAsync(x => _DBContext.Account.Remove(x));
+            await _DBContext.AccountType.ForEachAsync(x => _DBContext.AccountType.Remove(x));
+
             await _DBContext.RoleGrant.ForEachAsync(x => _DBContext.RoleGrant.Remove(x));
             await _DBContext.RolePermission.ForEachAsync(x => {
                 if (!x.IsSystemRolePermission){
@@ -57,6 +60,8 @@ namespace AgileStudioCLI.Commands
                     _DBContext.Permission.Remove(x);
                 }
             });
+            await _DBContext.User.ForEachAsync(x => _DBContext.User.Remove(x));
+
             _DBContext.SaveChanges();
         }
     }

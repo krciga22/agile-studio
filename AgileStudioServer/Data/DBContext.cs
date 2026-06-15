82c299f891f03fe9b3a4ffc20b4a5a6455a0f8f6
Andrew <krciga22@gmail.com>
2026-06-15 11:59:35 -0500

    [server] Add permission for account owners to read, update, and delete backlog item types

--- AgileStudioServer/Data/DBContext.cs
+++ AgileStudioServer/Data/DBContext.cs
@@ -329,6 +329,21 @@ namespace AgileStudioServer.Data
                 {
                     CreatedOn = new DateTime(2025, 6, 13, 0, 0, 0, DateTimeKind.Utc),
                     IsSystemRolePermission = true
+                },
+                new RolePermission(roleKey, PermissionKeys.READ, Scopes.ACCOUNT_BACKLOG_ITEM_TYPE)
+                {
+                    CreatedOn = new DateTime(2025, 6, 15, 0, 0, 0, DateTimeKind.Utc),
+                    IsSystemRolePermission = true
+                },
+                new RolePermission(roleKey, PermissionKeys.UPDATE, Scopes.ACCOUNT_BACKLOG_ITEM_TYPE)
+                {
+                    CreatedOn = new DateTime(2025, 6, 15, 0, 0, 0, DateTimeKind.Utc),
+                    IsSystemRolePermission = true
+                },
+                new RolePermission(roleKey, PermissionKeys.DELETE, Scopes.ACCOUNT_BACKLOG_ITEM_TYPE)
+                {
+                    CreatedOn = new DateTime(2025, 6, 15, 0, 0, 0, DateTimeKind.Utc),
+                    IsSystemRolePermission = true
                 }
             );
         }
---
+++ AgileStudioServer/Migrations/20260615165359_AddPermissionForAccountOwnerToReadUpdateAndDeleteBacklogItemTypes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AgileStudioServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissionForAccountOwnerToReadUpdateAndDeleteBacklogItemTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "security",
+                table: "role_permission",
+                columns: new[] { "permission_key", "role_key", "scope", "created_by_id", "created_on", "is_system_role_permission" },
+                values: new object[,]
+                {
+                    { "delete", "accounts-account-owner", "account.backlogItemType", null, new DateTime(2025, 6, 15, 0, 0, 0, 0, DateTimeKind.Utc), true },
+                    { "read", "accounts-account-owner", "account.backlogItemType", null, new DateTime(2025, 6, 15, 0, 0, 0, 0, DateTimeKind.Utc), true },
+                    { "update", "accounts-account-owner", "account.backlogItemType", null, new DateTime(2025, 6, 15, 0, 0, 0, 0, DateTimeKind.Utc), true }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "security",
+                table: "role_permission",
+                keyColumns: new[] { "permission_key", "role_key", "scope" },
+                keyValues: new object[] { "delete", "accounts-account-owner", "account.backlogItemType" });
+
+            migrationBuilder.DeleteData(
+                schema: "security",
+                table: "role_permission",
+                keyColumns: new[] { "permission_key", "role_key", "scope" },
+                keyValues: new object[] { "read", "accounts-account-owner", "account.backlogItemType" });
+
+            migrationBuilder.DeleteData(
+                schema: "security",
+                table: "role_permission",
+                keyColumns: new[] { "permission_key", "role_key", "scope" },
+                keyValues: new object[] { "update", "accounts-account-owner", "account.backlogItemType" });
+        }
+    }
+}

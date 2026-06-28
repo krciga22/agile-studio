b089afe01d9b5d0e4b73c62623de2f441045cc95
Andrew <krciga22@gmail.com>
2026-06-27 19:16:06 -0500

    [server] Add new method to get starting backlog item types for a given backlog item type schema

--- AgileStudioServer/Features/Accounts/BacklogItemTypes/BacklogItemTypeService.cs
+++ AgileStudioServer/Features/Accounts/BacklogItemTypes/BacklogItemTypeService.cs
@@ -1,6 +1,7 @@
 ﻿using AgileStudioServer.Core.Pagination;
 using AgileStudioServer.Core.Services;
 using AgileStudioServer.Core.Services.Exceptions;
+using AgileStudioServer.Features.Accounts.BacklogItemTypeSchemaEdges;
 using AgileStudioServer.Features.Resources.Resource;
 
 namespace AgileStudioServer.Features.Accounts.BacklogItemTypes
@@ -9,13 +10,17 @@ namespace AgileStudioServer.Features.Accounts.BacklogItemTypes
     {
         private readonly BacklogItemTypeRepository _BacklogItemTypeRepository;
 
+        private readonly BacklogItemTypeSchemaEdgeRepository _BacklogItemTypeSchemaEdgeRepository;
+
         private readonly ServiceContext _ServiceContext;
 
         public BacklogItemTypeService(
             BacklogItemTypeRepository backlogItemTypeRepository,
+            BacklogItemTypeSchemaEdgeRepository backlogItemTypeSchemaEdgeRepository,
             ServiceContext serviceContext)
         {
             _BacklogItemTypeRepository = backlogItemTypeRepository;
+            _BacklogItemTypeSchemaEdgeRepository = backlogItemTypeSchemaEdgeRepository;
             _ServiceContext = serviceContext;
         }
 
@@ -49,6 +54,24 @@ namespace AgileStudioServer.Features.Accounts.BacklogItemTypes
         public override int GetIdentifier(BacklogItemTypeModel backlogItemType)
         {
             return backlogItemType.ID;
+        }
+
+        public List<BacklogItemTypeModel> GetStartingBacklogItemTypesForSchema(int schemaId)
+        {
+            var edges = _BacklogItemTypeSchemaEdgeRepository.GetByFromTypeId(null, schemaId);
+
+            List<BacklogItemTypeModel> backlogItemTypes = [];
+            Dictionary<int, bool> backlogItemTypeIds = [];
+            foreach (var edge in edges)
+            {
+                if (backlogItemTypeIds.TryAdd(edge.ToTypeID, true))
+                {
+                    var backlogItemType = Get(edge.ToTypeID);
+                    backlogItemTypes.Add(backlogItemType);
+                }
+            }
+
+            return [.. backlogItemTypes];
         }
 
         public override BacklogItemTypeModel Create(BacklogItemTypeModel backlogItemType)
--- AgileStudioServerTest/IntegrationTests/Features/Accounts/BacklogItemTypes/BacklogItemTypeServiceTest.cs
+++ AgileStudioServerTest/IntegrationTests/Features/Accounts/BacklogItemTypes/BacklogItemTypeServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using AgileStudioServer.Core.Services.Exceptions;
+﻿using AgileStudioServer.Core.Services;
+using AgileStudioServer.Core.Services.Exceptions;
 using AgileStudioServer.Data;
 using AgileStudioServer.Features.Accounts.Accounts;
 using AgileStudioServer.Features.Accounts.BacklogItemTypes;
@@ -6,6 +7,7 @@ using AgileStudioServer.Features.Accounts.BacklogItemTypeSchemas;
 using AgileStudioServer.Features.Accounts.Workflows;
 using AgileStudioServerTest.Features.Accounts.Accounts;
 using AgileStudioServerTest.Features.Accounts.BacklogItemTypes;
+using AgileStudioServerTest.Features.Accounts.BacklogItemTypeSchemaEdges;
 using AgileStudioServerTest.Features.Accounts.BacklogItemTypeSchemas;
 using AgileStudioServerTest.Features.Accounts.Workflows;
 
@@ -19,23 +21,31 @@ namespace AgileStudioServerTest.IntegrationTests.Features.Accounts.BacklogItemTy
 
         private readonly BacklogItemTypeSchemaFixture _BacklogItemTypeSchemaFixture;
 
+        private readonly BacklogItemTypeSchemaEdgeFixture _BacklogItemTypeSchemaEdgeFixture;
+
         private readonly WorkflowFixture _WorkflowFixture;
 
         private readonly AccountFixture _AccountFixture;
+
+        private readonly ServiceContext _ServiceContext;
 
         public BacklogItemTypeServiceTest(
             DBContext dbContext,
             BacklogItemTypeService backlogItemTypeService,
             BacklogItemTypeFixture backlogItemTypeFixture,
             BacklogItemTypeSchemaFixture backlogItemTypeSchemaFixture,
+            BacklogItemTypeSchemaEdgeFixture backlogItemTypeSchemaEdgeFixture,
             WorkflowFixture workflowFixture,
-            AccountFixture accountFixture) : base(dbContext)
+            AccountFixture accountFixture,
+            ServiceContext serviceContext) : base(dbContext)
         {
             _backlogItemTypeService = backlogItemTypeService;
             _BacklogItemTypeFixture = backlogItemTypeFixture;
             _BacklogItemTypeSchemaFixture = backlogItemTypeSchemaFixture;
+            _BacklogItemTypeSchemaEdgeFixture = backlogItemTypeSchemaEdgeFixture;
             _WorkflowFixture = workflowFixture;
             _AccountFixture = accountFixture;
+            _ServiceContext = serviceContext;
         }
 
         [Fact]
@@ -63,6 +73,35 @@ namespace AgileStudioServerTest.IntegrationTests.Features.Accounts.BacklogItemTy
             Assert.NotNull(returnedBacklogItemType);
             Assert.Equal(backlogItemType.ID, returnedBacklogItemType.ID);
         }
+
+        [Fact]
+        public void GetStartingBacklogItemTypesForSchema_ReturnsBacklogItemTypesForSchema()
+        {
+            var account = _AccountFixture.Create();
+            var schema = _BacklogItemTypeSchemaFixture.Create(account: account);
+            var epicType = _BacklogItemTypeFixture.Create("Epic", account: account);
+            var storyType = _BacklogItemTypeFixture.Create("Story", account: account);
+            var defectType = _BacklogItemTypeFixture.Create("Defect", account: account);
+            var taskType = _BacklogItemTypeFixture.Create("Task", account: account);
+            var testType = _BacklogItemTypeFixture.Create("Test", account: account);
+
+            var epicEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, null, epicType);
+            var storyEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, null, storyType);
+            var defectEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, null, defectType);
+            var storyTaskEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, storyType, taskType);
+            var storyTestEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, storyType, testType);
+            var defectTaskEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, defectType, taskType);
+            var defectTestEdge = _BacklogItemTypeSchemaEdgeFixture.Create(schema, defectType, testType);
+
+            _ServiceContext.Sort = "id:asc";
+            var returnedBacklogItemTypes = _backlogItemTypeService.GetStartingBacklogItemTypesForSchema(schema.ID);
+
+            Assert.Collection(returnedBacklogItemTypes,
+                backlogItemType => Assert.Equal(epicType.ID, backlogItemType.ID),
+                backlogItemType => Assert.Equal(storyType.ID, backlogItemType.ID),
+                backlogItemType => Assert.Equal(defectType.ID, backlogItemType.ID));
+        }
+        
 
         [Fact]
         public void Update_ReturnsUpdatedBacklogItemType()
